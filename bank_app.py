fb195456fa56872794cad7f8aa0e9670b11d6a1e
clorieanne <clorieanne@gmail.com>
2014-12-02 13:39:25 +0800

    run standard name and made stop by Ctrl+C

--- bank_app.py
+++ bank_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from bank.bank import Bank
+from bank.account import Account
 
 app = Flask(__name__)
 BANK = Bank()
@@ -10,8 +11,8 @@ def hello_world():
     balance = BANK.get_account_balance(account_number)
     return render_template('index.html', balance=balance)
 
-
-
-
 if __name__ == '__main__':
-    app.run(debug = True)
+    import cProfile
+    account = Account('1111', 50)
+    BANK.add_account(account)
+    cProfile.run('app.run(debug = True)')
