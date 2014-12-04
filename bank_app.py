a5219a0fdee93ae4f2b14d2024eb508cbd640923
clorieanne <clorieanne@gmail.com>
2014-12-04 20:04:56 +0800

    reached 10/10

--- bank_app.py
+++ bank_app.py
@@ -1,18 +1,22 @@
+"""
+my mod
+"""
 from flask import Flask, render_template, request
 from bank.bank import Bank
 from bank.account import Account
 
-app = Flask(__name__)
+APP = Flask(__name__)
 BANK = Bank()
 
-@app.route('/')
+@APP.route('/')
 def hello_world():
+    """function"""
     account_number = request.args.get('account_number')
     balance = BANK.get_account_balance(account_number)
     return render_template('index.html', balance=balance)
 
 if __name__ == '__main__':
     import cProfile
-    account = Account('1111', 50)
-    BANK.add_account(account)
-    cProfile.run('app.run(debug = True)', sort = 'time')
+    ACCOUNT = Account('1111', 50)
+    BANK.add_account(ACCOUNT)
+    cProfile.run('APP.run(debug = True)', sort='time')
