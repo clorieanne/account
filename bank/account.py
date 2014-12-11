89d110802b1a5060e1a151aea7e6ca8e4d3c29e6
clorieanne <clorieanne@gmail.com>
2014-12-11 23:59:56 +0800

    necessary revisions for bank.account.py

--- bank/account.py
+++ bank/account.py
@@ -1,8 +1,10 @@
 class Account(object):
-  def __init__(self, account_number, balance):
-    self.account_number = account_number
-    self.balance = balance
+    """Initialize"""
+    def __init__(self, account_number, balance): #pragma: no cover
+      self.account_number = account_number
+      self.balance = balance
 
 class Withdraws(object):
-  def __init__( self, account_number,balance,withdraw):
-    self.balance = balance-withdraw
+    """For withdraws"""
+    def __init__(self, balance, withdraw): #pragma: no cover
+      self.balance = balance - withdraw
