da61100631efe5c2389953e5289c4b980e764923
clorieanne <clorieanne@gmail.com>
2014-12-12 11:32:36 +0800

    fixed errors in bank/bank.py and made it 10/10

--- bank/bank.py
+++ bank/bank.py
@@ -1,12 +1,18 @@
+"""Bank module"""
 class Bank(object):
-  def __init__(self):
-    self.accounts = {}
+    """Bank class"""
+    def __init__(self):
+        """Initialization"""
+        self.accounts = {}
 
-  def add_account(self, account):
-    self.accounts[account.account_number] = account.balance
+    def add_account(self, account):
+        """adding account"""
+        self.accounts[account.account_number] = account.balance
 
-  def get_account_balance(self, account_number): 
-    return self.accounts.get(account_number)
+    def get_account_balance(self, account_number):
+        """getting the balance"""
+        return self.accounts.get(account_number)
 
-  def account_exist(self, account_number):
-    self.accounts.get(account_number) != 'None'
+    def account_exist(self, account_number):
+        """check if account exist"""
+        return self.accounts.get(account_number) != 'None'
